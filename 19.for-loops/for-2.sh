e1536c19be73e1d2ad1d1b9e1b5fa0a7a3f054ac
kalyankalapala <kalyankalapala24@gmail.com>
2020-10-31 12:32:21 +0530

    while-for changes

--- 19.for-loops/for-2.sh
+++ 19.for-loops/for-2.sh
@@ -11,3 +11,13 @@ done
 for f in $( ls /var/ ); do
 	echo $f
 done 
+
+#changing the image from jpeg to jpg
+for file in $(ls images)
+do
+        if [[ $file = *.jpeg ]]
+                then
+                new_name=$(echo $file| sed 's/jpeg/jpg/g')
+                mv images/$file images/$new_name
+        fi
+done
